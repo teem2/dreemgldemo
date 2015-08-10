974444a01f5e390e7907493dac7eb2fb05f34a36
Stijn Kuipers <stijnkuipers@gmail.com>
2015-08-10 23:48:59 +0200

    more of the tweakings

--- classes/ruler.js
+++ classes/ruler.js
@@ -3,12 +3,17 @@
 
 define.class(function(sprite, text, view){
 	
+	this.vertical = false;
+	
+	this.offset = 20;
 	
 	this.ruler = function(){
 		
-		var dist =mesh.x *	width;
+		var dist =(mesh.x *	width) - offset;
 	
-		if (floor(mod(dist ,100.) ) < 1.) 
+		if (dist > 0.)
+		{ 
+			if(floor(mod(dist  ,100.) ) < 1.) 
 		{
 			return vec4(0.8,0.8,0.8,1);
 		}
@@ -16,14 +21,48 @@ define.class(function(sprite, text, view){
 		{
 			if (mesh.y >0.5 && floor(mod(dist ,10.) ) < 1.) 
 			{
-				return vec4(0.9,0.9,0.9,1.0);
+				return vec4(0.75,0.75,0.75,1.0);
 			}
-			return bgcolor;
 		}
-	}
-	this.render = function(){
+		}
+			return bgcolor;
+
+		}
+	
+	this.vruler = function(){
 		
-		var rulerres = view({bgcolor: "#b0b0b0", "bg.color": this.ruler, flexdirection:"column", flex: 1, alignself:"stretch" });;		
+		
+	var dist =(mesh.y *	height) - offset;
+	
+		if (dist > 0.)
+		{ 
+		
+		if (floor(mod(dist ,100.) ) < 1.) 
+		{
+			return vec4(0.8,0.8,0.8,1);
+		}
+		else
+		{
+			if (mesh.x >0.5 && floor(mod(dist ,10.) ) < 1.) 
+			{
+				return vec4(0.75,0.75,0.75,1.0);
+			}
+		}
+		}
+					return bgcolor;
+
+	}
+	
+	
+	this.render = function(){
+		if (this.vertical == false)
+		{
+			var rulerres = view({bgcolor: "#8080b0", "bg.color": this.ruler, flexdirection:"column","bg.offset": this.offset, flex: 1, alignself:"stretch" });;		
 		return rulerres;
+		}
+		else{
+			var rulerres = view({bgcolor: "#8080b0", "bg.color": this.vruler, flexdirection:"column", "bg.offset": this.offset,flex: 1, alignself:"stretch" });;		
+		return rulerres;
+		}
 	}
 })
--- compositions/treeviewtest/browser.js
+++ compositions/treeviewtest/browser.js
@@ -1,39 +1,27 @@
 define.browser(function(require, screen, view, edit, text, myview, treeview, ruler){
 	this.render = function(){return[
-		view({position: "relative",   flexdirection: "column", height: 20, bgcolor: "red", flex:1},[
-			view({position: "relative",   height: 50, flexdirection: "row", bgcolor: "#404080",'bg.color':function(){return vec4(mesh.x * bgcolor.rgb, 1.0)} , alignitems:"stretch", alignself: "stretch" , flex: undefined}
+		view({position: "relative",   flexdirection: "column", height: 20, bgcolor: "black", flex:1},[
+			view({position: "relative",  cornerradius:"vec4(0,0,0,25)" , borderwidth:1, height: 50, flexdirection: "row", bgcolor: "#404080",'bg.bgcolorfn':function(a,b){return vec4(mesh.x * bgcolor.rgb, 1.0)} , alignitems:"stretch", alignself: "stretch" , flex: undefined}
 				,text({fontsize: 26, text:"Visionary Studio 3016", width: 200, bgcolor: "transparent" , margin: 5})
 			)
-			,view({position: "relative",   flexdirection: "row", bgcolor: "#404080", alignitems:"stretch", alignself: "stretch" , flex:1}
-			,view({position: "relative", width: 400,  flexdirection: "column", bgcolor: "#b0b0b0", alignself: "stretch", borderwidth: 1, bordercolor: "gray" }
+			,view({position: "relative",   flexdirection: "row", bgcolor: "black", alignitems:"stretch", alignself: "stretch" , flex:1}
+			,view({position: "relative", width: 400,  flexdirection: "column", bgcolor: "#b0b0b0", alignself: "stretch", borderwidth: 1,cornerradius: "vec4(0,14,0,0)" , bordercolor: "gray" }
 				,treeview({})
 				)
-			,view({position: "relative", flex:0.34,  flexdirection: "column", bgcolor: "darkgray",padding: 1, borderwidth: 1, bordercolor: "gray" }
-				,ruler({height: 30})
-				,view({position: "relative", flex: 1.0, padding: 4, bgcolor: "#d0d0d0", alignitems: "stretch"}
-					,edit({fontsize:20, flex: 1.0,fgcolor: "black",'bg.color':function(){return vec4("#d0d0d0")} ,text:'Type\nHere\nMultiline'})
+			,view({position: "relative", flex:0.34,  flexdirection: "column", bgcolor: "darkgray",padding: 1, borderwidth: 1, bordercolor: "gray" , cornerradius: 0}
+				,ruler({height: 20})
+				,view({position: "relative", flex: 1.0, padding: 0, bgcolor: "#d0d0d0", alignitems: "stretch", flexdirection: "row" }
+					,ruler({width: 20, vertical: true, offset:0})
+					,view({position: "relative", flex: 1.0, padding: 4, bgcolor: "#d0d0d0", alignitems: "stretch"}
+				
+					,edit({fontsize:20, flex: 1.0,fgcolor: "black",'bg.color':function(){return vec4("#d0d0d0")} ,text:'Type\nHere\nMultiline'}))
 				)
 			)
-			,view({position: "relative", width: 400,  flexdirection: "column", bgcolor: "#b0b0b0", alignself: "stretch" , borderwidth: 1, bordercolor: "gray" })
-		),
-		
-		
-		]
-
-		
-		//,edit({x:100,y:30,w:300,h:80,fontsize:60, text:'MyValue'})
-
-			//view({x:0,y:0,w:100,h:100}))
-		
-		//,view({mode:'DOM', tag:'iframe', 'src':'http://blog.thisisnotrocketscience.nl', bgcolor:'yellow', x:10, y:100, w:190, h:190})
-		//,view({mode:'DOM', tag:'iframe', 'src':'http://blog.thisisnotrocketscience.nl', bgcolor:'blue', x:10, y:200, w:290, h:190})
-		//,treeview({})
-		
-		)
-		]
+			,view({position: "relative", width: 400,  flexdirection: "column", bgcolor: "#b0b0b0", alignself: "stretch" , borderwidth: 1, cornerradius: "vec4(0,0,14,0)" , bordercolor: "gray" })
+			),
+			])
+		]}
 	}
-	
-}
 );
 
 
