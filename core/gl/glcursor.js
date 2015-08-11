287e634a0816515bcb66a5331528f5f0b25b6f72
R Arends <rik@n4.io>
2015-08-11 15:28:31 +0200

    cursor

--- core/gl/glcursor.js
+++ core/gl/glcursor.js
@@ -13,7 +13,7 @@ define.class('$gl/glshader', function(require, exports, self){
 		var dpdy = dFdy(rel)
 		var edge = min(length(vec2(length(dpdx), length(dpdy))) * SQRT_1_2, 1.)
 		if(edge > 0.04){
-			if(rel.x < dpdx.x) return 'white'
+			if(rel.x < dpdx.x) return vec4(fgcolor.rgb,1.)
 			return vec4(0.)
 		}
 		return vec4(fgcolor.rgb, smoothstep(edge, -edge, shape.box(rel, 0,0,0.05,1.)))
@@ -27,8 +27,8 @@ define.class('$gl/glshader', function(require, exports, self){
 		this.addCursor = function(textbuf, start){
 			var pos = textbuf.cursorRect(start)
 			//pos.y = 0//this.textbuf.font_size - this.textbuf.font_size * this.textbuf.cursor_sink
-			pos.w = textbuf.font_size + 10;		
-			console.log("Rik - please find out why cursorwidth is near invisible at small fontsizes ;-) ");
+			pos.w = textbuf.font_size //+ 10;		
+			//console.log("Rik - please find out why cursorwidth is near invisible at small fontsizes ;-) ");
 			//this.cursor.mesh.length = 0
 			this.pushQuad(
 				pos.x, pos.y, 0, 0, 
