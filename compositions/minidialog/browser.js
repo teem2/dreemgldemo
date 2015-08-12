862b181da119b526c6ca1dfa61d60b86c131f290
Stijn Kuipers <stijnkuipers@gmail.com>
2015-08-12 15:38:45 +0200

    minidialog functional

--- compositions/minidialog/browser.js
+++ compositions/minidialog/browser.js
@@ -1,12 +1,15 @@
 define.browser(function(require, screen, view, edit, text, treeview){
 	this.render = function(){return[
-		view({},
-			view({},
-			edit({x:100,y:0,w:300,h:80,fontsize:60,text:'Type\nHere\nMultiline'}))
-				//,treeview({x:0,y:0})
+		view({flexdirection:"column" , flex: 1, bgcolor: "#808070"	},
+			view({bgcolor: "white" , flex: 1, margin: 40,flexdirection:"column" },
+				view({bgcolor: "#a0a070" , padding: 4,flexdirection:"column" },
+					text({text:"some dialog", bgcolor: "transparent" , fontsize: 20})
+				),
+				view({bgcolor: "#f0f0f0" , flex: 1, margin: 4,flexdirection:"column" },
+					edit({fontsize:30,text:'Type\nHere\nMultiline', flex: 1, 'bg.color':function(){return vec4("#f0f0f0")} , fgcolor: "black"})
+				)
 			)
-		
-		]
+		)]
 	}
 })
 
