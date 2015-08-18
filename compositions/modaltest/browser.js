54dc09cfe1ba34a109f6838b966b86c10b020948
Stijn Kuipers <stijnkuipers@gmail.com>
2015-08-18 02:11:50 +0200

    modal

--- compositions/modaltest/browser.js
+++ compositions/modaltest/browser.js
@@ -2,14 +2,14 @@ define.browser(function(require, screen, view, edit, text, treeview, splitcontai
 	this.render = function(){return[
 		view({flexdirection:"column" , flex: 1, bgcolor: "#708080"	},
 			view({bgcolor: "white" , flex: 1, margin: 140,flexdirection:"column" },
-				view({bgcolor: "#70a0a0" , padding: 4,flexdirection:"column" },
+				view({bgcolor: "#70a0a0" , padding: 4},
 					text({text:"Modal Dialog Test Setup 1", bgcolor: "transparent" , fontsize: 20})
 				),
 				tabcontrol({bgcolor: "#f0f0f0" }
 					,view({tabname: "thing 1" },
 						button({text:"Open Modal 1",icon:"youtube", click:
 							function(){
-								this.screen.modal(
+								this.screen.openModal(
 									modalview({x: this.mouse.x+100})
 								).then(
 									function(result){									
@@ -21,7 +21,7 @@ define.browser(function(require, screen, view, edit, text, treeview, splitcontai
 					,view({tabname: "thing 2" },
 						button({text:"Open Modal 2",icon:"youtube", click:
 							function(){
-								this.screen.modal(
+								this.screen.openModal(
 									modalview({x: 400})
 								).then(
 									function(result){									
--- compositions/modaltest/modalview.js
+++ compositions/modaltest/modalview.js
@@ -1,25 +1,23 @@
 // Copyright 2015 Teem2 LLC, MIT License (see LICENSE)
 // mouse debug class
 
-define.class(function(sprite, text, view){
-	
+define.class(function(sprite, text, view, button){
 	this.position = "absolute" ;
 	this.left = 100;
 	this.top = 100;
 	this.render = function(){
 		this.margin=  140;
-		this.flexdirection = "column" ;
-		
-		return [view({bgcolor: "white" , flex: 1, flexdirection:"column" }
-						,view({bgcolor: "#70a0a0" , padding: 4,flexdirection:"column" },
-							text({text:"Modal Dialog!", bgcolor: "transparent" , fontsize: 16})
+		this.flexdirection = "column";
+		return 
+			[view({bgcolor: "white" , flex: 1, flexdirection:"column" }
+						,view({bgcolor: "#70a0a0" , padding: 4,flexdirection:"column" ,flexdirection:"row", aligncontent:"flex-end"  }
+							,text({text:"Modal Dialog!", bgcolor: "transparent" , fontsize: 16, flex: 1})
+							,button({icon:"close", alignitem: "flex-end" })
 						)
 						,view({},
 							text({text:"I am a modal!", fontsize: 20, margin: 20,fgcolor: "black" })
 						)
 					)
-					];
+			];
 	}
-	
-	
 });
