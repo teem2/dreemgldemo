8406a0de3833476993cb675c90c438d6d4b2680c
Stijn Kuipers <stijnkuipers@gmail.com>
2015-08-23 23:22:56 +0200

    flowgraph styling

--- compositions/flowgraph/browser.js
+++ compositions/flowgraph/browser.js
@@ -368,9 +368,38 @@ define.browserClass(function(require,screen, node, datatracker, spline, cadgrid,
 		this.attribute("text", {type:String, value:""});
 		this.attribute("title", {type:String, value:""});
 		this.attribute("input", {type:boolean, value:""});
+		this.attribute("basecolor", {type:vec4, value: vec4("green")});
 		this.attribute("icon", {type:string, value:"flask"});
 		this.attribute("targetscreen", {type:String, value:""});
 		this.attribute("attrib", {type:String, value:""});
+		this.flex = 1.0;
+		this.alignself = "stretch"
+		this.flexdirection = "row"
+		
+		this.bg.basecolor = vec4();
+		this.bg.direction  = 1;
+		this.bg.offset  = 0;
+		this.bg.bgcolorfn = function(a,b){
+
+			
+			
+			
+			return mix(vec4(1,1,1,0), basecolor,  offset +  a.x * direction);
+		
+		
+		}
+		
+		this.atDraw = function(){
+			if (this.input === false){
+				this.bg.direction = 1;
+				this.bg.offset = 0;
+			}else {
+				this.bg.direction = -1;
+				this.bg.offset = 1;
+			}
+			
+			this.bg.basecolor = this.basecolor;
+		}
 		
 		this.mouseover = function(){
 			this.setDirty();
@@ -388,13 +417,29 @@ define.browserClass(function(require,screen, node, datatracker, spline, cadgrid,
 					this.target.setConnectionStart(this.targetscreen, this.attrib);
 			}
 		}
+		this.borderwidth = 2;
+		this.bordercolor ="white" ;
 		
 		this.render =function(){		
 			if (this.input){
-				return [icon({icon: this.icon, margin: vec4(10,0,10,0), fontsize: 14}),text({text:this.title,margin: vec4(10,0,10,4), fontsize: 14, bgcolor:"transparent", fgcolor:"black"})];
+				this.justifycontent = "flex-start"
+			this.cornerradius = vec4(12,12,0,0);
+		
+				return [
+							view({bgcolor: "white", padding: vec4(4,0,4,2), cornerradius: 10,borderwidth: 2,bordercolor:"white",margin: vec4(3,3,0,3)},
+								icon({icon: this.icon, fontsize: 14})),
+								text({text:this.title,margin:vec4(4,2,0,0),fontsize: 14, bgcolor:"transparent", fgcolor:"#404040"})
+							];
 			}
 			else{
-				return [text({text:this.title, margin: vec4(10,0,10,4), fontsize: 14, bgcolor:"transparent", fgcolor:"#404040"}), icon({icon: this.icon,margin:vec4(10,0,10,0), fontsize: 14})];
+				this.justifycontent = "flex-end"
+				this.cornerradius = vec4(0,0,12,12);
+		
+				return [
+							text({text:this.title, margin: vec4(4,2,16,0), fontsize: 14, bgcolor:"transparent", fgcolor:"#404040"}), 
+								view({bgcolor: "white", padding: vec4(4,0,4,2), cornerradius: 10,borderwidth: 2,bordercolor:"white" ,  margin: vec4(0,3, 3,3)},
+								icon({icon: this.icon, fontsize: 14}))
+							];
 			}
 		}
 	})
@@ -488,12 +533,12 @@ define.browserClass(function(require,screen, node, datatracker, spline, cadgrid,
 				for (var i in this.data.linkables){
 					var L = this.data.linkables[i];				
 					if (L.input == true)	{
-						var newinput = connectorbutton({title: L.title, icon: L.icon, text:L.name,input: true, target: this.parent, targetscreen: this.data.name, attrib:L.name})
+						var newinput = connectorbutton({basecolor: this.data.basecolor, title: L.title, icon: L.icon, text:L.name,input: true, target: this.parent, targetscreen: this.data.name, attrib:L.name})
 						this.inputsdict[L.name] = newinput;
 						this.inputs.push(newinput);
 					}
 					else{
-						var newoutput = connectorbutton({title: L.title, icon: L.icon, text:L.name,input: false,target: this.parent, targetscreen: this.data.name, attrib:L.name})
+						var newoutput = connectorbutton({basecolor: this.data.basecolor,title: L.title, icon: L.icon, text:L.name,input: false,target: this.parent, targetscreen: this.data.name, attrib:L.name})
 						this.outputsdict[L.name] = newoutput;
 						this.outputs.push(newoutput);					
 					}
