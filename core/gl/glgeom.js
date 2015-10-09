fb860afcd2ef997951b1294089bd798526f6fab4
R Arends <rik@n4.io>
2015-10-09 15:22:04 +0200

    Step1

--- core/gl/glgeom.js
+++ core/gl/glgeom.js
@@ -1,11 +1,22 @@
 // MIT License. 
 
 define(function(require, exports){	
-	
-	// Use material-capture "lit sphere" texture to approximate material behaviour for a surface.
-	exports.createBox = function(addtriangle, width, height, depth){					
-	}
+	/*
+	define.struct({
+		pos:vec3
+	})
 
+	createCube(10,10,10,function(id, p1, p2, p3, n1, n2, n3, t1, t2, t3){
+		buf.push(p1, n1, t1)
+		buf.push(p2, n2, t2)
+		buf.push(p3, n3, t3)
+	})*/
+
+	exports.createCube = function(width, height, depth, cb){
+		if(arguments.length === 2) cb = height, height = depth = width
+		if(arguments.length === 3) cb = depth, depth = height, height = width
+	}
+	
 	exports.createSphere = function(addtriangle, radius, detaillevel){			
 	}
 }
