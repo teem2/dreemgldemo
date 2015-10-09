a3649129f7e558470e62600d293ac595097ca6a5
Stijn Kuipers <stijnkuipers@gmail.com>
2015-10-09 15:04:31 +0200

    more gl functions - glgeom.js for geometry generators

---
+++ core/gl/glgeom.js
@@ -0,0 +1,11 @@
+// MIT License. 
+
+define(function(require, exports){	
+	
+	// Use material-capture "lit sphere" texture to approximate material behaviour for a surface.
+	exports.createBox = function(addtriangle, width, height, depth){					
+	}
+
+	exports.createSphere = function(addtriangle, radius, detaillevel){			
+	}
+}
--- core/gl/glmaterial.js
+++ core/gl/glmaterial.js
@@ -1,16 +1,11 @@
 // MIT License. 
 
-define(function(require, exports){
-	exports.noise = require('./glnoise')
-	exports.pal = require('./glpalette')
-	exports.math = require('./glmath')
-
+define(function(require, exports){	
+	// Use material-capture "lit sphere" texture to approximate material behaviour for a surface.
 	exports.matcap = function(texture, eye, normal){	
 		var r = reflect( eye, normal );
 		var m = 2. * sqrt( pow( r.x, 2. ) + pow( r.y, 2. ) + pow( r.z + 1., 2. ) );
 		var vN = r.xy / m + .5;		
 		return texture2D( texture, vN );
-	}
-	
-	
+	}	
 })
