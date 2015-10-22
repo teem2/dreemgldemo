ad94ded615d6910a560fce9306dc7070a6528357
Stijn Kuipers <stijnkuipers@gmail.com>
2015-10-22 22:09:58 +0200

    material progress

--- compositions/geometrytest/geomtest.js
+++ compositions/geometrytest/geomtest.js
@@ -5,12 +5,13 @@ define.class(function(require, screen, view, text, cube, perspective3d, teapot, 
 	
 	this.render = function(){
 		return [
-			view({alignitems:"stretch",flexdirection:"column" , flex: 0.3, bgcolor:"transparent", bg:{bgcolorfn:function(a,b){return vec4("black" ) + (1-a.y) * vec4("#005060");}}}
-				,perspective3d({ bgcolor:"transparent", aligself:"stretch", clipping: true, fov:45, camera:[0,0,-100], lookat:[0,0,0],margin:vec4(0), borderwidth:0, bordercolor:"lightgray"}, 
-					button({text:"I am a button in a 3d viewport" })
-				)
+			view({alignitems:"stretch",flexdirection:"column" , flex: 0.3, bgcolor:"transparent", bg:{bgcolorfn:function(a,b){var p = pow(a.y, 3.0);return vec4("rgb(0,24,172)")*p + (1-p) * vec4("rgb(0,0,17)");}}}
+//				,perspective3d({ bgcolor:"transparent", aligself:"stretch", clipping: true, fov:45, camera:[0,0,-100], lookat:[0,0,0],margin:vec4(0), borderwidth:0, bordercolor:"lightgray"}, 
+	//				button({text:"I am a button in a 3d viewport" })
+		//		)
 				,view({flexdirection:"row", flex: 1, bgcolor:"transparent"}
-					,perspective3d({flex: 1, bgcolor:"transparent", aligself:"stretch", clipping: true, fov:45, camera:[53,-10,-53], lookat:[0,0,0],margin:vec4(0), borderwidth:0, bordercolor:"lightgray"}, 
+					,perspective3d({flex: 1,name:"moleculesview", bgcolor:"transparent", aligself:"stretch", clipping: true, fov:45, camera:[53,-10,-53], lookat:[0,0,0],margin:vec4(0), borderwidth:0, bordercolor:"lightgray"}, 
+						ballrotate({init:function(){this.target= this.find("moleculesview");}, width:100, height:100})	,
 						model({file:"molecule.obj", rot3d:[PI,PI/2,0],scale3d:[0.2,0.2,0.2],pos3d:[-1,-15,0]}),
 						model({file:"molecule.obj", rot3d:[PI,PI/2 + 0.1,0],scale3d:[0.2,0.2,0.2],pos3d:[-1,-10,0]}),
 						model({file:"molecule.obj", rot3d:[PI,PI/2 + 0.2,0],scale3d:[0.2,0.2,0.2],pos3d:[-1,-5,0]}),
@@ -27,9 +28,9 @@ define.class(function(require, screen, view, text, cube, perspective3d, teapot, 
 								}						
 							}, rot3d:[PI/2,0,0], pos3d:[0,1,0]})
 					)
-					,perspective3d({name:"rotatableview", flex: 1, bgcolor:"transparent", aligself:"stretch", clipping: true, borderwidth:12, bordercolor:"black",fov:40, camera:[0,0,-100]}
+					,perspective3d({name:"teapotview", flex: 1, bgcolor:"transparent", aligself:"stretch", clipping: true, borderwidth:12, bordercolor:"black",fov:40, camera:[0,0,-100]}
 						
-						,ballrotate({init:function(){this.target= this.find("rotatableview");}, width:100, height:100})	
+						,ballrotate({init:function(){this.target= this.find("teapotview");}, width:100, height:100})	
 						,teapot({position:"absolute" , radius: 1, detail:4, pos3d:[10,0,10], rot3d:[PI/2,0,0]})
 						,teapot({radius: 1, detail:4, pos3d:[20,0,20], rot3d:[PI/2,0,0]})
 						,teapot({radius: 1, detail:4, pos3d:[30,0,30], rot3d:[PI/2,0,0]})
@@ -44,17 +45,20 @@ define.class(function(require, screen, view, text, cube, perspective3d, teapot, 
 					)
 					
 				)
-				,view({flex: 1, bgcolor:"rgba(40,220,255,150)", bordercolor:"transparent", flexdirection:"row" }
-				,perspective3d({clipping: true, flex: 1, bgcolor:"transparent" }
-					, cube({bgcolor: "black", pos3d:[1,0,0], dimension:[0.2,0.2,0.2], rot3d:[.0,.1,0]})				
-					, cube({bgcolor: "black", pos3d:[2,0,0], dimension:[0.2,0.5,0.2], rot3d:[.1,.1,0]})				
-					, cube({bgcolor: "black", pos3d:[3,0,0], dimension:[0.2,0.8,0.2], rot3d:[.2,.1,0]})				
-					, cube({bgcolor: "black", pos3d:[4,0,0], dimension:[0.2,1.1,0.2], rot3d:[.3,.1,0]})				
-					, cube({bgcolor: "black", pos3d:[5,0,0], dimension:[0.2,1.4,0.2], rot3d:[.4,.1,0]})				
-					, cube({bgcolor: "black", pos3d:[0,0,0], dimension:[0.2,0.5,0.2], rot3d:[-.1,.1,0]})				
-				)
-				,perspective3d({flex:1, clipping: true, bgcolor:"transparent"}, 
-					cube({bgcolor: "black", rot3d:[2,1,0]}))
+				,view({flex: 1, bgcolor:"transparent", flexdirection:"row" }
+					,perspective3d({name:"cubesview", clipping: true, flex: 1, bgcolor:"transparent" }
+						, ballrotate({init:function(){this.target= this.find("cubesview");}, width:100, height:100})	
+						, cube({bgcolor: "black", pos3d:[1,0,0], dimension:[0.2,0.2,0.2], rot3d:[.0,.1,0]})				
+						, cube({bgcolor: "black", pos3d:[2,0,0], dimension:[0.2,0.5,0.2], rot3d:[.1,.1,0]})				
+						, cube({bgcolor: "black", pos3d:[3,0,0], dimension:[0.2,0.8,0.2], rot3d:[.2,.1,0]})				
+						, cube({bgcolor: "black", pos3d:[4,0,0], dimension:[0.2,1.1,0.2], rot3d:[.3,.1,0]})				
+						, cube({bgcolor: "black", pos3d:[5,0,0], dimension:[0.2,1.4,0.2], rot3d:[.4,.1,0]})				
+						, cube({bgcolor: "black", pos3d:[0,0,0], dimension:[0.2,0.5,0.2], rot3d:[-.1,.1,0]})				
+					)
+					,perspective3d({name:"singlecube", flex:1, clipping: true, bgcolor:"transparent"}
+						, ballrotate({init:function(){this.target= this.find("singlecube");}, width:100, height:100})	
+						, cube({bgcolor: "black", rot3d:[2,1,0]})
+					)
 				)
 			)
 		]
