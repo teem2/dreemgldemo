b137add7d035b2f110445183a83d29a5ceb2462b
Stijn Kuipers <stijnkuipers@gmail.com>
2015-11-03 16:13:14 +0100

    specdoc

--- core/draw/layer.js
+++ core/draw/layer.js
@@ -4,23 +4,32 @@
 define.class(function(){
 // Baseclass for all implementations of the layer subsystem
 
+// Layer should render its output to 1 or more hidden buffers.
+// the parent of the layer is responsible for displaying the content somehow. This could be a deferred rendering setup, an appleblur or a straight bitblt.
+
 // Layer has 2 modes:
 // - 2d
 // - 3d
 
-// the layer 2d mode provides:
-// - flexbox layout
+// The layer 2d mode provides:
+// - flexbox layout engine
 // (- dirtyrect handling?)
 // - an orthagonal projection matrix
 
-
 // the layer 3d mode provides:
-// - a perspective matrix
-// - 
+// - a perspective matrix based on:
+//		- ortho/perspective mode
+//		- FOV
+//		- lookat position
+//		- camera position
+//		- up vector
 
-// - a projection matrix
-// - a camera
-// - a layout engine for 
+// - draw order based on:
+// 		- front to back / grouped by material for solid materials
+//		- back to front for transparent objects
+
+// "passes" array - could be interesting for deferred/ui picking setups.
+
 
 	
 	
