cc19f81b90f22f09678a74a1b0ac7648484c224e
Stijn Kuipers <stijnkuipers@gmail.com>
2015-11-03 16:04:04 +0100

    empty baseclasses

---
+++ core/draw/device.js
@@ -0,0 +1,13 @@
+// Copyright 2015 Teem2 LLC, MIT License (see LICENSE)
+// Base class for all implementations of the device subsystem
+
+
+// a device should:
+// - open a drawing context for a single "screen" in the composition
+// - provide user-interface hooks (mouse/keyboard) 
+// - provide a frame-hook for accurate tear-free display & animations
+
+define.class(function(){
+
+	
+});
---
+++ core/draw/layer.js
@@ -0,0 +1,27 @@
+// Copyright 2015 Teem2 LLC, MIT License (see LICENSE)
+
+
+define.class(function(){
+// Baseclass for all implementations of the layer subsystem
+
+// Layer has 2 modes:
+// - 2d
+// - 3d
+
+// the layer 2d mode provides:
+// - flexbox layout
+// (- dirtyrect handling?)
+// - an orthagonal projection matrix
+
+
+// the layer 3d mode provides:
+// - a perspective matrix
+// - 
+
+// - a projection matrix
+// - a camera
+// - a layout engine for 
+
+	
+	
+})
